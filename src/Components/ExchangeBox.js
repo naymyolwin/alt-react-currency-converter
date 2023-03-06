bf9ce9cf15cf555b4a22f39c6d38cfa389fde635
Nay Myo Lwin <naymyolwin@Nays-MacBook-Pro.local>
2023-03-06 22:28:29 +0700

    change input the dropdown

--- src/Components/ExchangeBox.js
+++ src/Components/ExchangeBox.js
@@ -8,8 +8,11 @@ class ExchangeBox extends Component {
           className="w-100 bg-secondary position-absolute top-0 start-0 "
           style={{ height: "400px", zIndex: "-1" }}
         ></div>
-        <div className="" style={{ height: "100px" }}></div>
-        <div className="bg-info m-5 rounded-5" style={{ height: "400px" }}>
+        <div className="" style={{ height: "125px" }}></div>
+        <div
+          className="bg-info m-auto my-5 rounded-5 shadow"
+          style={{ height: "300px", maxWidth: "770px", minWidth: "570px" }}
+        >
           {/* <div className="h-100 d-flex align-items-center">
             <div className="bg-success input-group w-100 px-5 justify-content-between">
               <input
@@ -50,23 +53,49 @@ class ExchangeBox extends Component {
 
             <button className="btn btn-danger">Convert</button>
           </div> */}
-          <div className="row h-100 w-100">
-            <div className="row d-flex align-items-end gutter-0">
-              <div className="input-group w-100 px-5 justify-content-between">
+          <div className="row h-100 w-100 gutter-0 m-0">
+            <div className="row d-flex align-items-end gutter-0 m-0">
+              <div className="input-group w-100 m-0 px-5 justify-content-between">
                 <input
+                  required
+                  type="text"
+                  className="form-control m-2"
+                  placeholder="Amount"
+                />
+                <div className="dropdown m-2">
+                  <button
+                    className="btn btn-secondary dropdown-toggle bg-light"
+                    type="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Dropdown button
+                  </button>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                {/* <input
+                  required
                   type="text"
                   className="form-control m-2"
                   placeholder="From"
-                />
-                <input
-                  type="text"
-                  className="form-control m-2"
-                  placeholder="To"
-                />
-                <button
-                  className="btn btn-light rounded-circle m-2"
-                  type="button"
-                >
+                /> */}
+                <button className="btn btn-light rounded-5 m-2" type="button">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -74,8 +103,9 @@ class ExchangeBox extends Component {
                     stroke-width="2.5"
                     stroke="currentColor"
                     style={{
-                      width: "24px",
-                      height: "24px",
+                      margin: "auto",
+                      width: "20px",
+                      height: "20px",
                       color: "#BB4430",
                     }}
                   >
@@ -86,14 +116,42 @@ class ExchangeBox extends Component {
                     />
                   </svg>
                 </button>
-                <input
+                <div className="dropdown m-2">
+                  <button
+                    className="btn btn-secondary dropdown-toggle bg-light"
+                    type="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Dropdown button
+                  </button>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                {/* <input
+                  required
                   type="text"
                   className="form-control m-2"
-                  placeholder=""
-                />
+                  placeholder="To"
+                /> */}
               </div>
             </div>
-            <div className="row d-flex align-items-center gutter-0">
+            <div className="row d-flex align-items-center gutter-0 m-0">
               <div className="col d-flex justify-content-end pe-5">
                 <button className="btn btn-danger btn-lg">Convert</button>
               </div>
