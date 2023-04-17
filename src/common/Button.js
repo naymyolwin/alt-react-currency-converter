e1c9095b6dc3142a18354cda98419aea79c043cb
Nay Myo Lwin <naymyolwin@Nays-MacBook-Pro.local>
2023-04-17 11:07:08 +0700

    Navbar UI complete

---
+++ src/common/Button.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Button extends Component {
+  render() {
+    return (
+      <div className="hidden md:flex justify-center items-center px-5 py-3 bg-ccOrange  rounded-md text-ccGray">
+        Contact Us
+      </div>
+    );
+  }
+}
+
+export default Button;
--- src/common/Hamburger.js
+++ src/common/Hamburger.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Hamburger extends Component {
   render() {
     return (
-      <div className="relative flex flex-col ">
+      <div className="relative flex flex-col md:hidden ">
         <span className=" hamburger-top w-10 h-1 bg-ccGray" />
         <span className="hamburger-middle my-2 w-10 h-1 bg-ccGray" />
         <span className="hamburger-bottom w-10 h-1 bg-ccGray" />
