a7d4ea3949f6217e427363449f64de78f9f695e3
Nay Myo Lwin <naymyolwin@Nays-MacBook-Pro.local>
2023-04-17 14:05:21 +0700

    button hover effect add

--- src/common/Button.js
+++ src/common/Button.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Button extends Component {
   render() {
     return (
-      <div className="hidden md:flex justify-center items-center px-5 py-3 bg-ccOrange  rounded-md text-ccGray">
+      <div className="hidden md:flex justify-center items-center px-5 py-3 bg-ccOrange rounded-md text-ccGray cursor-pointer border-2 border-ccOrange hover:bg-ccGray hover:text-ccOrange duration-200">
         Contact Us
       </div>
     );
--- src/common/ConvertBtn.js
+++ src/common/ConvertBtn.js
@@ -3,9 +3,9 @@ import React, { Component } from "react";
 class ConvertBtn extends Component {
   render() {
     return (
-      <button className="px-5 py-3 bg-ccOrange text-center rounded-md text-ccGray w-32 md:w-full max-w-xs md:self-end">
+      <div className="px-5 py-3 bg-ccOrange text-center rounded-md text-ccGray w-32 md:w-full max-w-xs md:self-end cursor-pointer border-2 border-ccOrange hover:bg-ccGray hover:text-ccOrange">
         Convert
-      </button>
+      </div>
     );
   }
 }
