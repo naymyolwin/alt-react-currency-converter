406daecd0b3623016fc73b2c996f61d5b0541095
Nay Myo Lwin <naymyolwin@Nays-MacBook-Pro.local>
2023-04-29 22:54:36 +0700

    card transition updated

--- src/common/Card.js
+++ src/common/Card.js
@@ -4,7 +4,7 @@ class Card extends Component {
   render() {
     return (
       <div>
-        <div className="relative flex flex-col w-96 bg-white rounded-lg shadow-xl p-7 h-72 hover:-top-1 hover:shadow-2xl transition-all duration-400">
+        <div className="relative flex flex-col w-96 bg-white rounded-lg shadow-xl p-7 h-72 transition-all  hover:-translate-y-2 hover:shadow-2xl duration-300">
           <div className="font-bold text-ccDark">{this.props.title}</div>
           <div className="text-sm my-5 text-ccDark">{this.props.text}</div>
           <div className="text-ccTeal mt-auto">{this.props.name}</div>
