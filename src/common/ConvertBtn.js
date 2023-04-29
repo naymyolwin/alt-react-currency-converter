56ebcc6de0f3aece4819889213cbbb4d99bf76b6
Nay Myo Lwin <naymyolwin@Nays-MacBook-Pro.local>
2023-04-29 13:13:17 +0700

    convert button condition added

--- src/common/ConvertBtn.js
+++ src/common/ConvertBtn.js
@@ -2,11 +2,18 @@ import React, { Component } from "react";
 
 class ConvertBtn extends Component {
   render() {
+    const result =
+      this.props.amount === "" ||
+      this.props.fromCurrency === "" ||
+      this.props.toCurrency === ""
+        ? false
+        : true;
     return (
       <div
-        className="px-5 py-3 bg-ccOrange text-center rounded-lg text-ccGray w-32 md:w-full max-w-xs md:self-end cursor-pointer border-2 border-ccOrange shadow-lg hover:bg-ccGray hover:text-ccOrange"
+        className={`px-5 py-3 bg-ccOrange text-center rounded-lg text-ccGray w-32 md:w-full max-w-xs md:self-end cursor-pointer border-2 border-ccOrange shadow-lg hover:bg-ccGray hover:text-ccOrange ${
+          result ? "pointer-events-auto" : "pointer-events-none"
+        }`}
         onClick={this.props.onConvert}
-        pointerEvents="none"
       >
         Convert
       </div>
